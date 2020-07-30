f28286a817eb09eca8fe4aa619c87527fd5d74c3
Rafael pena <rafael.pena@visie.com.br>
2020-07-30 00:59:44 -0300

    Usando conceito de refatoracao para melhor o metodo converter da classe ConversorDeNumeroRomano

--- src/CDC/Exemplos/ConversorDeNumeroRomano.php
+++ src/CDC/Exemplos/ConversorDeNumeroRomano.php
@@ -4,12 +4,20 @@ namespace CDC\Exemplos;
 
 class ConversorDeNumeroRomano
 {
+    protected $tabela = [
+        "I" => 1,
+        "V" => 5,
+        "X" => 10,
+        "L" => 50,
+        "C" => 100,
+        "D" => 500,
+        "M" => 1000
+    ];
+
     public function converter($numeroEmRomano)
     {
-        if ($numeroEmRomano === "V") {
-            return 5;
+        if (array_key_exists($numeroEmRomano, $this->tabela)) {
+            return $this->tabela[$numeroEmRomano];
         }
-
-        return 1;
     }
 }

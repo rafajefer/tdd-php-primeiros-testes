f1b0689b6fe2f114625a6dc08087a6ecf1830edd
Rafael pena <rafael.pena@visie.com.br>
2020-07-30 22:30:44 -0300

    Fazendo o teste passar da maneira mais simples possivel

---
+++ src/CDC/Loja/RH/CalculadoraDeSalario.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CDC\Loja\RH;
+
+use CDC\Loja\RH\Funcionario;
+
+class CalculadoraDeSalario
+{
+    public function calcularSalario(Funcionario $funcionario)
+    {
+        return 1350.0;
+    }
+}

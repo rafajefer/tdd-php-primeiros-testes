430e63df04f0e1e5bf90a2535695de5d645d7e65
Rafael pena <rafael.pena@visie.com.br>
2020-07-30 22:28:04 -0300

    Criando teste para calcula salario de desenvolvedor com salario abaixo do limite

---
+++ tests/CDC/Loja/RH/CalculadoraDeSalarioTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace CDC\Loja\RH;
+
+require "./vendor/autoload.php";
+
+use PHPUnit\Framework\TestCase as PHPUnit;
+use CDC\Loja\RH\Funcionario, CDC\Loja\RH\TabelaCargos, CDC\Loja\RH\CalculadoraDeSalario;
+
+class CalculadoraDeSalarioTest extends PHPUnit
+{
+    public function testCalculoSalarioDesenvolvedoresComSalarioAbaixoDoLimite()
+    {
+        $calculadora = new CalculadoraDeSalario();
+        $desenvolvedor = new Funcionario("Rodrigo", 1500.0, TabelaCargos::DESENVOLVEDOR);
+        $salario = $calculadora->calcularSalario($desenvolvedor);
+        $this->assertEquals(1500.0 * 0.9, $salario, null, 0.00001);
+    }
+}

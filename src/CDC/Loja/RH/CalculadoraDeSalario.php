741bbdb597b011ac5af0ce3be7d0936415cc7c0e
Rafael pena <rafael.pena@visie.com.br>
2020-07-30 22:36:22 -0300

    Adicionando a regra de negocio do exercicio

--- src/CDC/Loja/RH/CalculadoraDeSalario.php
+++ src/CDC/Loja/RH/CalculadoraDeSalario.php
@@ -4,6 +4,16 @@ namespace CDC\Loja\RH;
 
 use CDC\Loja\RH\Funcionario;
 
+/**
+ * Regra básica de negócio
+ * 
+ * Desenvolvedores possuem 20% de desconto caso seus salários sejam
+ * maiores do que R$ 3000,0. Caso contrário, o desconto é de 10%.
+ * 
+ * DBAs e testadores possuem desconto de 25% se seus salários forem
+ * maiores do que R$ 2500,0. 15%, em caso contrário.
+ * 
+ */
 class CalculadoraDeSalario
 {
     public function calcularSalario(Funcionario $funcionario)
@@ -11,7 +21,7 @@ class CalculadoraDeSalario
         if ($funcionario->getSalario() > 3000) {
             return 3200.00;
         }
-        
+
         return 1350.0;
     }
 }
